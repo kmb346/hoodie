b09f93b6d6e6342a8bdeab8fea0c9466c4f6fc5f
Kevin <kmb346@yahoo.com>
2025-03-01 14:30:17 +0900

    build error resolving

--- src/app/ConvexClientProvider.tsx
+++ src/app/ConvexClientProvider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ConvexReactClient, ConvexProvider } from "convex/react";
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 import { env } from "~/env";
  
 const convex = new ConvexReactClient(env.NEXT_PUBLIC_CONVEX_URL!);
