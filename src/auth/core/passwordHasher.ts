d8022b2aaa8849939c0181b5ae483e5fedc99517
Kevin <kmb346@yahoo.com>
2025-03-23 16:18:38 +0900

    revised hashPassword function

--- src/auth/core/passwordHasher.ts
+++ src/auth/core/passwordHasher.ts
@@ -4,12 +4,9 @@ import bcrypt from "bcryptjs";
 export function hashPassword(password: string, salt: string):Promise<string> {
   return new Promise((resolve, reject) => {
     bcrypt.hash(password.normalize(), salt, (error, hash) => {
-      if (error) {
-        reject("Unable to hash password");
-      }
-      if (hash) {
-        resolve(hash.normalize());
-      }
+      if (error) reject(error);
+      
+      if (hash) resolve(hash.normalize());
     });
   })
 }
