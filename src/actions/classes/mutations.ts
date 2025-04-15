c8bd30dd01615fb3dd65ce08f750a66c83497dfa
Kevin <kmb346@yahoo.com>
2025-04-15 22:03:54 +0900

    edit to time field in mutations

--- src/actions/classes/mutations.ts
+++ src/actions/classes/mutations.ts
@@ -7,7 +7,7 @@ export async function createClass(data: ClassSchema) {
   const location = await fetchMutation(api.mutations.class.createClass, {
     name: data.name,
     def_day: data.def_day ?? "",
-    def_time: data.def_time ?? 0,
+    def_time: data.def_time ?? "",
     teacher_id: data.teacher_id ?? undefined,
     student_limit: data.student_limit ?? 0,
   });
